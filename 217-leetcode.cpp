f0d8e0c03b21714b8c21dc7bf0bbc95bfb5607e0
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-02-05 23:18:37 +0530

    Entry-15. This is leetcode question 217. I used introsort and basic loop for this. Time complexoty is O(n logn) due to sorting taking time

---
+++ 217-leetcode.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    
+    for(int i = 0; i < n - 1; i++) {
+    if(arr[i] == arr[i + 1]) {
+        cout << "true";
+        return 0;
+    }
+}
+cout << "false";
+
+    return 0;
+}
+
