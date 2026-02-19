23f1201531a22e8ff5522aa0e87f68c047a8e8b4
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-02-19 06:28:36 +0000

    Code challenge submission for question: 349 Intersection of Two Arrays

---
+++ 349 Intersection of Two Arrays/349intersection-of-two-arrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set <int> s;
+        vector<int> res;
+        int l=0;
+        while(l<nums1.size()){
+            s.insert(nums1[l]);
+            l++;
+        }
+
+        for(int i=0;i<nums2.size();i++){
+            if(s.find(nums2[i])!=s.end()){
+                res.push_back(nums2[i]);
+                s.erase(nums2[i]);
+            }
+        }
+        return res;
+    }
+
+};
