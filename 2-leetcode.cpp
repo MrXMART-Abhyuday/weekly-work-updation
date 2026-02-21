9ef5422b3d9ddd0e452b64ab3d8094b73904558b
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-02-22 01:58:31 +0530

    Entry-32. This is leetcode 2. Solved previously but forgot to upload.

--- 2-leetcode.cpp
+++ 2-leetcode.cpp
@@ -1,60 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// class Node{
-//     public:
-//     int data;
-//     Node* next;
-//     Node(int val){
-//         data=val;
-//         next=NULL;
-//     }
-// };
-
-// class LinkedList{
-//     Node* head;
-//     Node* tail;
-    
-//     public:
-//     LinkedList(){
-//         head=tail=NULL;
-//     }
-
-//     void pushfront(int val){
-//         Node* newNode= new Node(val);//dynamic
-//         if(head==NULL){
-//             head= newNode;
-//             tail= newNode;
-//         }
-//         else{
-//             newNode->next=head;
-//             head=newNode;
-//         }
-//     }
-//     void print(){
-//         Node* temp;
-//         temp=head;
-//         while(temp!=NULL){
-//             cout<<temp->data<<" ";
-//             temp=temp->next;
-//         }
-//         cout<<endl;
-//     }
-
-//     void pushback(int val){
-//         Node* newNode= new Node(val);
-//         //head==tail==NULL
-//         if(tail==NULL){
-//             head=newNode;
-//             tail=newNode;
-//         }
-//         else{
-//             tail->next=newNode;
-//             newNode->next= NULL;
-//             tail=newNode;
-//         }
-//     }
-// };
 int main() {
     int n;
     cin>>n;
