f5e2e929a476bda8a74702d99fba38e4d0b25aa3
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-05-21 10:39:49 +0000

    Code challenge submission for question: 1295 Find Numbers with Even Number of Digits

---
+++ 1295 Find Numbers with Even Number of Digits/1295find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count=0;
+    for(int i=0;i<nums.size();i++){
+        int len = to_string(nums[i]).length();
+        if(len%2==0){
+            count++;
+        }
+    }
+return count;
+    }
+};
