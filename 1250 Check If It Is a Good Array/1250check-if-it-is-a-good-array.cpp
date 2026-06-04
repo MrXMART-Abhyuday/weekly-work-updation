dfba0965a619bbebeb5a026197e231d0590fb9ed
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-06-04 21:47:22 +0000

    Code challenge submission for question: 1250 Check If It Is a Good Array

---
+++ 1250 Check If It Is a Good Array/1250check-if-it-is-a-good-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int g=nums[0];
+        for(int i=1;i<nums.size();i++) {
+            g=__gcd(g, nums[i]);
+        }
+        if (g==1) {
+            return true;
+            }
+    return false;
+    }
+};
