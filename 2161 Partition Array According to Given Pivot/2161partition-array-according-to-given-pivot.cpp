aa7a8c5f7ef98638566e09cffbf6b090b6689133
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-06-08 20:58:59 +0000

    Code challenge submission for question: 2161 Partition Array According to Given Pivot

---
+++ 2161 Partition Array According to Given Pivot/2161partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> left;
+        vector<int> right;
+        vector<int> equal;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot) left.push_back(nums[i]);
+            if(nums[i]>pivot) right.push_back(nums[i]);
+            if(nums[i]==pivot) equal.push_back(nums[i]);
+        }
+        vector<int> ans;
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.insert(ans.end(), equal.begin(), equal.end());
+        ans.insert(ans.end(), right.begin(), right.end());
+
+return ans;
+    }
+};
