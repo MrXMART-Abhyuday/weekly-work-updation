d8892104998b60bb5b8ccb166d9f198d9a724eee
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-06-27 19:19:16 +0000

    Code challenge submission for question: 1337 The K Weakest Rows in a Matrix

---
+++ 1337 The K Weakest Rows in a Matrix/1337the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+
+        vector<pair<int,int>> rows;
+
+        for(int i = 0; i < mat.size(); i++) {
+
+            int soldiers = 0;
+
+            for(int j = 0; j < mat[i].size(); j++) {
+                if(mat[i][j] == 1)
+                    soldiers++;
+            }
+
+            rows.push_back({soldiers, i});
+        }
+
+        sort(rows.begin(), rows.end());
+
+        vector<int> ans;
+
+        for(int i = 0; i < k; i++) {
+            ans.push_back(rows[i].second);
+        }
+
+        return ans;
+    }
+};
