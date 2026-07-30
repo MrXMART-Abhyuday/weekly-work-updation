c9402aa509a074cbeb7874cf2191c8824d209849
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-07-30 14:09:28 +0530

    CodeForces 271A

---
+++ A_Beautiful_Year.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int year;
+    cin >> year;
+
+    while (true) {
+        year++;
+
+        int a = year / 1000;
+        int b = (year / 100) % 10;
+        int c = (year / 10) % 10;
+        int d = year % 10;
+
+        if (a != b && a != c && a != d &&
+            b != c && b != d &&
+            c != d) {
+            cout << year;
+            break;
+        }
+    }
+
+    return 0;
+}
