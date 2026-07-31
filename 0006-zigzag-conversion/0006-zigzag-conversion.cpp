c62952b242f95f241ba7153ef5ccd50d07ad9e8c
Abhyuday Anand <anandabhi2105@gmail.com>
2026-07-31 15:43:02 +0530

    0006-ZIGZAG-CONVERSION Stats: Time: 7 ms (40.74%), Space: 14.2 MB (25.09%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1 || numRows >= s.length())
-            return s;
-
+        if(s.size()<numRows || numRows==1) return s;
+        bool down=true;
+        int row=0;
         vector<string> rows(numRows);
-
-        int row = 0;
-        bool down = true;
-
-        for (char c : s) {
-            rows[row] += c;
-
-            if (row == 0)
-                down = true;
-            else if (row == numRows - 1)
-                down = false;
-
-            if (down)
-                row++;
-            else
-                row--;
+        for(char c:s){
+            rows[row]+=c;
+            if(row==0) down=true;
+            else if(row==numRows-1) down=false;
+            if(down) row++;
+            else row--;
         }
-
-        string ans = "";
-        for (string str : rows)
-            ans += str;
-
-        return ans;
+        string ans="";
+        for(string str:rows){
+            ans+=str;
+        }
+return ans;
     }
 };
