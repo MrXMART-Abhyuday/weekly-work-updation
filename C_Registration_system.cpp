d16f39f4b32e483db0432c4acd0aade45263c7b2
MrXMART-Abhyuday <anandabhi2105@gmail.com>
2026-08-17 10:53:02 +0530

    codeforces 4c

---
+++ C_Registration_system.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<string, int> mp;
+
+    while (n--) {
+        string s;
+        cin >> s;
+
+        if (mp.find(s) == mp.end()) {
+            cout << "OK" << endl;
+            mp[s] = 1;
+        }
+        else {
+            cout << s << mp[s] << endl;
+            mp[s]++;
+        }
+    }
+
+    return 0;
+}
